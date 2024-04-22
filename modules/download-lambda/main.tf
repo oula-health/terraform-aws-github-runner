d2398d7ad4114cff4fe3c9eaff460c59ed66df41
Mike Wolonkiewicz <michal@oulahealth.com>
2024-04-22 16:58:09 +0200

    modules/download-lambda: make it terragrunt friendly

--- modules/download-lambda/main.tf
+++ modules/download-lambda/main.tf
@@ -4,10 +4,11 @@ resource "null_resource" "download" {
   triggers = {
     name = var.lambdas[count.index].name
     file = "${var.lambdas[count.index].name}.zip"
+    filename = var.lambdas[count.index].filename
     tag  = var.lambdas[count.index].tag
   }
 
   provisioner "local-exec" {
-    command = "curl -o ${self.triggers.file} -fL https://github.com/philips-labs/terraform-aws-github-runner/releases/download/${self.triggers.tag}/${self.triggers.name}.zip"
+    command = "curl -o ${self.triggers.filename} -fL https://github.com/philips-labs/terraform-aws-github-runner/releases/download/${self.triggers.tag}/${self.triggers.name}.zip"
   }
 }
